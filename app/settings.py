abb2a912444795ac37ca3d90befb2cb1d888ba3c
peterwade153 <peterwade153@gmail.com>
2022-08-04 20:21:24 +0300

    allowed hosts

--- app/settings.py
+++ app/settings.py
@@ -26,7 +26,7 @@ SECRET_KEY = os.environ.get('SECRET_KEY', '-3hx#1mbzna8divpx^ytpw51ih+d*@7#s$lhn
 # SECURITY WARNING: don't run with debug turned on in production!
 DEBUG = True
 
-ALLOWED_HOSTS = []
+ALLOWED_HOSTS = ['*']
 
 
 # Application definition
